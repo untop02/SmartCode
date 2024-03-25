5b3ba3c9584f4ee03370d6e5c741e998a6188837
Glatus <janipudas1@gmail.com>
2024-03-25 17:22:34 +0200

    test

--- out/extension.js
+++ out/extension.js
@@ -27,21 +27,37 @@ exports.deactivate = exports.activate = void 0;
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = __importStar(require("vscode"));
+const fs = __importStar(require("fs"));
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "firstextension" is now active!');
+    console.log('Congratulations, your extension "Smart Code" is now active!');
+    let consoleChannel = vscode.window.createOutputChannel("Console");
     // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
+    // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('firstextension.helloWorld', () => {
+    let disposable = vscode.commands.registerCommand("firstextension.smartCode", function () {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from firstExtension!');
+        vscode.window.showInformationMessage("Smart Code go BRR");
+        //Creates new Tab
+        const panel = vscode.window.createWebviewPanel("SmartCode", "Visual Studio Text Extension", vscode.ViewColumn.One, { enableScripts: true });
+        panel.webview.html = getWebContent(context);
+        panel.webview.onDidReceiveMessage((message) => {
+            consoleChannel.append(message);
+            if (message.command === "alert") {
+                vscode.window.showInformationMessage(message.text);
+            }
+        });
     });
     context.subscriptions.push(disposable);
+    function getWebContent(context) {
+        const htmlPath = vscode.Uri.joinPath(context.extensionUri, "src\\extension.html");
+        const htmlContent = fs.readFileSync(htmlPath.fsPath, "utf8");
+        return htmlContent;
+    }
 }
 exports.activate = activate;
 // This method is called when your extension is deactivated
--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,55 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as fs from "fs";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "Smart Code" is now active!');
+  let consoleChannel = vscode.window.createOutputChannel("Console");
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "firstextension.smartCode",
+    function () {
+      // The code you place here will be executed every time your command is executed
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "firstextension" is now active!');
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Smart Code go BRR");
+      //Creates new Tab
+      const panel = vscode.window.createWebviewPanel(
+        "SmartCode",
+        "Visual Studio Text Extension",
+        vscode.ViewColumn.One,
+        { enableScripts: true }
+      );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('firstextension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from firstExtension!');
-	});
+      panel.webview.html = getWebContent(context);
 
-	context.subscriptions.push(disposable);
+      panel.webview.onDidReceiveMessage((message) => {
+        consoleChannel.append(message);
+        if (message.command === "alert") {
+          vscode.window.showInformationMessage(message.text);
+        }
+      });
+    }
+  );
+  context.subscriptions.push(disposable);
+
+  function getWebContent(context: vscode.ExtensionContext): string {
+    const htmlPath = vscode.Uri.joinPath(
+      context.extensionUri,
+      "src\\extension.html"
+    );
+
+    const htmlContent = fs.readFileSync(htmlPath.fsPath, "utf8");
+
+    return htmlContent;
+  }
 }
 
 // This method is called when your extension is deactivated
