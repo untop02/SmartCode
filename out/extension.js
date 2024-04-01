729ef56997ea294b5c375ffe56c5bc4881d4c5a9
Nipa <Niilo.urpola@metropolia.fi>
2024-04-01 10:40:03 +0300

    Lisäsin paremmman näkösen logo vaihtoehdo koska hyi

--- out/extension.js
+++ out/extension.js
@@ -4,66 +4,86 @@ exports.deactivate = exports.activate = void 0;
 const vscode = require("vscode");
 const openai_1 = require("openai");
 function activate(context) {
-    console.log('Congratulations, your extension "Smart Code" is now active!');
-    const provider = new SmartCodeProvider(context.extensionUri);
-    context.subscriptions.push(vscode.window.registerWebviewViewProvider(SmartCodeProvider.viewType, provider));
-    context.subscriptions.push(vscode.commands.registerCommand("smartCode.openView", () => {
-        vscode.commands.executeCommand("smartCode.codeView.focus");
-    }));
+  console.log('Congratulations, your extension "Smart Code" is now active!');
+  const provider = new SmartCodeProvider(context.extensionUri);
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider(
+      SmartCodeProvider.viewType,
+      provider
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand("smartCode.openView", () => {
+      vscode.commands.executeCommand("smartCode.codeView.focus");
+    })
+  );
 }
 exports.activate = activate;
 class SmartCodeProvider {
-    _extensionUri;
-    static viewType = "smartCode.codeView";
-    _view;
-    constructor(_extensionUri) {
-        this._extensionUri = _extensionUri;
-    }
-    resolveWebviewView(webviewView, context, _token) {
-        this._view = webviewView;
-        webviewView.webview.options = {
-            enableScripts: true,
-            localResourceRoots: [this._extensionUri],
-        };
-        let consoleChannel = vscode.window.createOutputChannel("Console");
-        webviewView.webview.html = this.getWebContent(webviewView.webview);
-        webviewView.webview.onDidReceiveMessage((message) => {
-            consoleChannel.append(message);
-            if (message.command === "alert") {
-                this.api(message.text);
-                vscode.window.showInformationMessage(message.text !== "" ? message.text : "No input :(");
-            }
-        });
-    }
-    openai = new openai_1.default({ baseURL: "http://boysedating.ddns.net:1234/v1", apiKey: "lm-studio" });
-    history = [
-        { "role": "system", "content": "You are an intelligent assistant. You always provide well-reasoned answers that are both correct and helpful." },
-    ];
-    async api(input) {
-        if (input !== "") {
-            var usrInput = { "role": "user", "content": input };
-            this.history.push(usrInput);
-            const completion = await this.openai.chat.completions.create({
-                messages: this.history,
-                model: "gpt-3.5-turbo",
-                response_format: { type: "json_object" },
-                stream: true,
-            });
-            var new_message = { "role": "assistant", "content": "" };
-            for await (const chunk of completion) {
-                if (chunk.choices[0].delta.content) {
-                    new_message.content += chunk.choices[0].delta.content;
-                    console.log(new_message.content);
-                    this._view?.webview.postMessage({ response: new_message.content });
-                }
-            }
-            this.history.push(new_message);
+  _extensionUri;
+  static viewType = "smartCode.codeView";
+  _view;
+  constructor(_extensionUri) {
+    this._extensionUri = _extensionUri;
+  }
+  resolveWebviewView(webviewView, context, _token) {
+    this._view = webviewView;
+    webviewView.webview.options = {
+      enableScripts: true,
+      localResourceRoots: [this._extensionUri],
+    };
+    let consoleChannel = vscode.window.createOutputChannel("Console");
+    webviewView.webview.html = this.getWebContent(webviewView.webview);
+    webviewView.webview.onDidReceiveMessage((message) => {
+      consoleChannel.append(message);
+      if (message.command === "alert") {
+        this.api(message.text);
+        vscode.window.showInformationMessage(
+          message.text !== "" ? message.text : "No input :("
+        );
+      }
+    });
+  }
+  openai = new openai_1.default({
+    baseURL: "http://boysedating.ddns.net:1234/v1",
+    apiKey: "lm-studio",
+  });
+  history = [
+    {
+      role: "system",
+      content:
+        "You are an intelligent assistant. You always provide well-reasoned answers that are both correct and helpful.",
+    },
+  ];
+  async api(input) {
+    if (input !== "") {
+      var usrInput = { role: "user", content: input };
+      this.history.push(usrInput);
+      const completion = await this.openai.chat.completions.create({
+        messages: this.history,
+        model: "gpt-3.5-turbo",
+        response_format: { type: "json_object" },
+        stream: true,
+      });
+      var new_message = { role: "assistant", content: "" };
+      for await (const chunk of completion) {
+        if (chunk.choices[0].delta.content) {
+          new_message.content += chunk.choices[0].delta.content;
+          console.log(new_message.content);
+          this._view?.webview.postMessage({ response: new_message.content });
         }
+      }
+      this.history.push(new_message);
     }
-    getWebContent(webview) {
-        const styleUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "out", "style.css"));
-        const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "out", "script.js"));
-        return `<!DOCTYPE html>
+  }
+  getWebContent(webview) {
+    const styleUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "out", "style.css")
+    );
+    const scriptUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "out", "script.js")
+    );
+    return `<!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
@@ -75,8 +95,16 @@ class SmartCodeProvider {
         <script>const vscode = acquireVsCodeApi();
         </script>
             <header>
+            <--- Niilon parempi logo
+            <img src="../img/Smartcode_logo.jpg"
+                alt="" srcset="" width="200">
+            --->
+
+            <--- Unton Logo 
                 <img src="https://i.imgur.com/kycO1SS.gif"
                     alt="" srcset="" width="300">
+            --->
+            
                 <p id='p1'>This has some text to show to the user</p>
                 <script>
         const display = document.getElementById('p1');
@@ -103,8 +131,8 @@ class SmartCodeProvider {
             <script src="${scriptUri}"></script>
         </body>
         </html>`;
-    }
+  }
 }
-function deactivate() { }
+function deactivate() {}
 exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
