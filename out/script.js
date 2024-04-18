6bd8feb715955a29a529ef18b9509d3edd68fd79
Unto Pulkkinen <untop@metropolia.fi>
2024-04-18 23:20:52 +0300

    markdown parsing with marked.js

--- out/script.js
+++ out/script.js
@@ -6,54 +6,51 @@ const inputField = document.getElementById("uInput");
 const copyButton = document.getElementById("copyButton");
 let text = document.getElementById("p1");
 function getState() {
-  return JSON.parse(localStorage.getItem("smartCodeState") ?? "");
+    return JSON.parse(localStorage.getItem("smartCodeState") ?? "");
 }
 function setState(newState) {
-  localStorage.setItem("smartCodeState", JSON.stringify(newState));
+    localStorage.setItem("smartCodeState", JSON.stringify(newState));
 }
 //Pitää ehkä säätää vielä koska poistaa
 function initializeState() {
-  const currentState = getState();
-  inputField.value = currentState;
+    const currentState = getState();
+    inputField.value = currentState;
 }
 function sendMessage() {
-  vscode.postMessage({ command: "alert", text: inputField.value });
-  inputField.value = "";
-  vscode.postMessage({ command: "alert", text: inputField.value });
-  inputField.value = "";
+    vscode.postMessage({ command: "alert", text: inputField.value });
+    inputField.value = "";
 }
 function clearHistory() {
-  vscode.postMessage({ command: "clear" });
+    vscode.postMessage({ command: "clear" });
 }
 sendButton?.addEventListener("click", () => {
-  sendMessage();
+    sendMessage();
 });
 clearButton?.addEventListener("click", () => {
-  clearHistory();
+    clearHistory();
 });
 async function setClipboard(text) {
-  const type = "text/plain";
-  const blob = new Blob([text], { type });
-  const data = [new ClipboardItem({ [type]: blob })];
-  await navigator.clipboard.write(data);
+    const type = "text/plain";
+    const blob = new Blob([text], { type });
+    const data = [new ClipboardItem({ [type]: blob })];
+    await navigator.clipboard.write(data);
 }
 document?.addEventListener("keypress", (event) => {
-  if (event.key === "Enter" && event.shiftKey !== true) {
-    sendMessage();
-  } else if (event.key === "Enter" && event.shiftKey === true) {
-    const inputText = inputField.value;
-    inputText.concat("\n");
-  }
+    if (event.key === "Enter" && event.shiftKey !== true) {
+        sendMessage();
+    }
+    else if (event.key === "Enter" && event.shiftKey === true) {
+        const inputText = inputField.value;
+        inputText.concat("\n");
+    }
 });
 document.addEventListener("DOMContentLoaded", () => {
-  initializeState();
-  initializeState();
+    initializeState();
 });
 document.getElementById("uInput")?.addEventListener("change", () => {
-  const inputText = document.getElementById("uInput").value;
-  setState(inputText);
+    const inputText = document.getElementById("uInput")
+        .value;
+    setState(inputText);
 });
-copyButton?.addEventListener("click", () =>
-  setClipboard(text?.textContent ?? "")
-);
+copyButton?.addEventListener("click", () => setClipboard(text?.textContent ?? ""));
 //# sourceMappingURL=script.js.map
